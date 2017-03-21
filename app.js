4f52f8c0591d2d1ce47e9d0282a71a771baa0990
Skylake <skylakecoder@gmail.com>
2017-03-21 19:08:24 +0800

    temp-commit.

--- app.js
+++ app.js
@@ -6,9 +6,57 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
+var LockFrameServer = {
+    _clientId: 0,
+    _clientSocketList: [],
+    _clientCount: 2,
+    _currentReadyCount: 0,
+    _clientReadyRecord: {},
+
+    _generateClientId: function() {
+        return this._clientId++;
+    },
+
+    AddClient: function(clientSocket) {
+        clientSocket.socketId = this._generateClientId();
+        this._clientSocketList.push(clientSocket);
+    },
+
+    RemoveClient: function(clientSocket) {
+        var index = this._clientSocketList.indexOf(clientSocket);
+        if (index != -1) {
+            this._clientSocketList.splice(index, 1);
+            delete this._clientReadyRecord[clientSocket.socketId];
+            if (this._currentReadyCount > 0) {
+                this._currentReadyCount--;
+            }
+        }
+    },
+
+    OnClientReady: function(clientSocket) {
+        if (this._clientReadyRecord[clientSocket.socketId]) {
+            console.log("[error] client already ready..." + clientSocket.socketId);            
+            return;
+        }
+        this._clientReadyRecord[clientSocket.socketId] = true;
+        this._currentReadyCount++;
+        if (this._currentReadyCount >= this._clientCount) {
+            console.log("all ready!!");
+            this._clientSocketList.forEach(function(socket) {
+                socket.emit("start", { fps: 30, keyFrame: 5 });
+            }, this);
+        }
+    }
+};
+
 io.on('connection', function(socket) {
-    socket.emit('res', { hello : 'world' });
-    socket.on('req', function(data) {
-        console.log(data);
+    LockFrameServer.AddClient(socket);
+    socket.emit('connected',  {});
+    socket.on('ready', function(data) {
+        LockFrameServer.OnClientReady(socket);
+    });
+    socket.on('disconnect', function(data) {
+        console.log('client disconnected: ' + data);
+        LockFrameServer.RemoveClient(socket);
     });
 });
